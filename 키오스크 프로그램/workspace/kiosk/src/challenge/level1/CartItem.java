253cac0406f36735dc1299fb2348cbad82dc86a9
Hyunmin <khmin052@naver.com>
2025-04-28 21:05:56 +0900

    20250428_21시00분 수정

---
+++ 키오스크 프로그램/workspace/kiosk/src/challenge/level1/CartItem.java
@@ -0,0 +1,26 @@
+package challenge.level1;
+
+public class CartItem {
+
+    // 속성
+    private MenuItem menuItem;
+    private int quantity;
+
+    // 생성자
+    public CartItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+        this.quantity = 1; // 처음 담을때 수량
+    }
+
+    // 기능
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    // 메뉴 갯수 증가
+    public void increaseQuantity() {
+        quantity++;
+    }
+}
---
+++ 키오스크 프로그램/workspace/kiosk/src/challenge/level1/Menu.java
@@ -0,0 +1,46 @@
+package challenge.level1;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    //속성
+    private List<MenuItem> menuList = new ArrayList<>();
+
+    // 생성자
+    public Menu() {
+
+    }
+
+    //기능
+    public void addMenu(MenuItem item) {
+        menuList.add(item);
+    }
+
+    public void printMenu() {
+        System.out.println("[ SHAKESHACK MENU ]");
+        for (MenuItem item : menuList) {
+            System.out.println(item.getItemNum() + ". " + item.getMenuName() + " | " + "W " + item.getPrice() + " | " + item.getIntroduce());
+        }
+        System.out.println(0 + ". 돌아가기      | 돌아가기");
+    }
+
+    public MenuItem findMenu(int menuNum) {
+        for (MenuItem item : menuList) {
+            if (item.getItemNum() == menuNum) {
+                return item;
+            }
+        }
+        throw new RuntimeException();
+        // return null; // 수정
+    }
+
+    // 게터
+    public List<MenuItem> getMenuList() {
+        return menuList;
+    }
+
+
+    // 세터
+}
